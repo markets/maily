6e0355bf14606569ccef4138e76178689c7d3a9f
Marc Anguera Insa <srmarc.ai@gmail.com>
2018-06-15 01:04:25 +0200

    new logo :art: + frontend cleanup and minor enhancements

--- app/helpers/maily/application_helper.rb
+++ app/helpers/maily/application_helper.rb
@@ -1,7 +1,21 @@
 module Maily
   module ApplicationHelper
+    def title
+      _title = 'Maily'
+
+      if params[:mailer] && params[:email]
+        _title << " - #{params[:mailer].humanize} | #{params[:email].humanize}"
+      end
+
+      _title
+    end
+
     def sidebar_class(mailer, email)
       'maily_selected_mail' if mailer.name == params[:mailer] && email.name == params[:email]
+    end
+
+    def icon(name)
+      image_tag "maily/icons/#{name}.svg", class: 'icon'
     end
   end
 end
--- app/helpers/maily/emails_helper.rb
+++ app/helpers/maily/emails_helper.rb
@@ -4,6 +4,15 @@ module Maily
       mailers.map { |mailer| mailer.emails.size }.sum
     end
 
+    def email_description(email)
+      return unless email.description
+
+      content_tag(:div, class: 'maily_description') do
+        concat content_tag(:strong, 'Description ')
+        concat email.description
+      end
+    end
+
     def part_class(part)
       'format_selected' if (part == params[:part]) || (part == 'html' && !params[:part])
     end
