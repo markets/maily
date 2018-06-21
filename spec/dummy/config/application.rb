c942387a9cca6d6ce46512eeaabee8d35fb8d634
Marc Anguera Insa <srmarc.ai@gmail.com>
2018-06-21 20:49:33 +0200

    explicitly require all maily assets

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,7 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'action_controller/railtie'
+require 'action_view/railtie'
 require 'action_mailer/railtie'
+require 'sprockets/railtie'
 
 Bundler.require(*Rails.groups)
 
