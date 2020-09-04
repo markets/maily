0c92527e2c960020dcde053ef386ab096bb6a4b9
Barry Woolgar <barry@storm-consultancy.com>
2020-09-04 18:33:32 +0100

    Avoid the dependency on Sprockets by inlining image assets and converting SCSS to CSS
    This now makes Maily compatible with Webpacker/API projects by
    not requiring Sprockets be loaded in the parent application.

--- lib/maily/engine.rb
+++ lib/maily/engine.rb
@@ -2,12 +2,5 @@ module Maily
   class Engine < ::Rails::Engine
     isolate_namespace Maily
     load_generators
-
-    config.assets.precompile << %w(
-      maily/application.css
-      maily/logo.png
-      maily/icons/globe.svg
-      maily/icons/paperclip.svg
-    )
   end
 end
